fd78c761a5e99003da7f1b1865008537245af26d
lee <lijinqiu5@foxmail.com>
2014-01-17 09:26:06 +0800

    Initial Commit

---
+++ PRL-1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PRL-1
+//
+//  Created by sy on 14-1-17.
+//  Copyright (c) 2014年 sy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PRL-1Tests/PRL_1Tests.m
@@ -0,0 +1,34 @@
+//
+//  PRL_1Tests.m
+//  PRL-1Tests
+//
+//  Created by sy on 14-1-17.
+//  Copyright (c) 2014年 sy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PRL_1Tests : XCTestCase
+
+@end
+
+@implementation PRL_1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
